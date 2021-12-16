05e2a4ea17824b22f097c90d59a1224b359a9d21
SomuTakuli <78639651+SomuTakuli@users.noreply.github.com>
2021-12-16 19:31:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,14 +106,14 @@ async def my_event_handler(event):
             hit = hit.hit.split(":")
             
             button = [
-                [Button.url("Authentication error?", "https://t.me/nordbypass")],
+                [Button.url("Authentication error?", "https://t.me/IgnitedCoders")],
                 [(Button.inline("Report not working", data=f"report_{hitID}"))]
             ]
             await bot.send_message(event.chat_id, genMsg.format(account = Var.account_name, email = hit[0], pwd = hit[1], name = first_name, channelName = Var.channelName), buttons = button)
         else:
             await bot.send_message(event.chat_id, "No account available right now.")
     if '/start' == event.raw_text.lower():
-        button = [[(Button.url("Repo Link", "https://github.com/leeveshkamboj/TGAccountGeneratorBot"))]]
+        button = [[(Button.url("Ignited Network", "https://t.me/IgnitedNetwork"))]]
         await bot.send_message(event.chat_id, startMsg.format(name = first_name), buttons=button)
         return
     if event.chat_id in Var.ownerIDs:
